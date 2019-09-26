12bb96f300fd79924151a115e518019f886f9da3
Lucas Souza <lucassouza690@gmail.com>
2019-09-26 20:25:11 -0300

    criacao model

---
+++ SimpleAPI/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleAPI.Models
+{
+    public class Usuario
+    {
+        [Key, Required, Display(Name = "id")]
+        public int Id { get; set; }
+        [Display(Name = "cpf")]
+        public string Cpf { get; set; }
+        [Display(Name = "fullname")]
+        public string Fullname { get; set; }
+        [Display(Name = "emailaddress")]
+        public string Emailaddress { get; set; }
+        [Display(Name = "datanascimento")]
+        public string Datanascimento { get; set; }
+    }
+}
