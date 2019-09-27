911a4171db047ad21556a15804067a5436be7614
Lucas Souza <lucassouza690@gmail.com>
2019-09-27 20:26:57 -0300

    redireciona para swagger

--- SimpleAPI/App_Start/WebApiConfig.cs
+++ SimpleAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Swashbuckle.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -21,6 +22,13 @@ namespace SimpleAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Routes.MapHttpRoute(
+               name: "swagger_root",
+               routeTemplate: "",
+               defaults: null,
+               constraints: null,
+               handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+
             
         }
     }
